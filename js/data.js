6ae0fec68b8ced39ba153dd3e4864eafea61cd59
saslamp <sadiqasg@gmail.com>
2019-09-25 12:54:03 +0100

    > renamed tabs to reflect CSOs called remaining data and populated the details page

--- js/data.js
+++ js/data.js
@@ -1,5 +1,6 @@
 (function ($) {
-
+    // published and submited
+    // no report
     $(function () {
 
         $.ajax({
@@ -10,6 +11,7 @@
                     break;
                 }
 
+                // destructureout sections to be used
                 // let bpsr = result["BUREAU OF PUBLIC SERVICE REFORMS (BPSR)"]
                 let { "BUREAU OF PUBLIC SERVICE REFORMS (BPSR)": bpsr } = result;
                 let { pro_dis, foi_officers, data, rank } = bpsr;
@@ -29,15 +31,15 @@
                     }
                 }
                 // brw
-                let { lvl_dis: brw_dis, foi_training: brw_foi_tr } = rank[0].ranking
+                let { lvl_dis: brw_dis, foi_training: brw_foi_tr, level_response: brw_duration } = rank[0].ranking
                 lvlDis(brw_dis, "#brw-dis")
 
                 // ppdc
-                let { lvl_dis: ppdc_dis, foi_training: ppdc_foi_tr } = rank[1].ranking
+                let { lvl_dis: ppdc_dis, foi_training: ppdc_foi_tr, level_response: ppdc_duration } = rank[1].ranking
                 lvlDis(ppdc_dis, "#ppdc-dis")
 
                 // budgit
-                let { lvl_dis: budgit_dis, foi_training: budgit_foi_tr } = rank[2].ranking
+                let { lvl_dis: budgit_dis, foi_training: budgit_foi_tr, level_response: budgit_duration } = rank[2].ranking
                 lvlDis(budgit_dis, "#budgit-dis")
 
                 // 
@@ -64,6 +66,57 @@
                     $("#foi-desk-officers").html('<div class="bg-danger w-6 h-6 rounded-circle"></div><p>None</p>')
                 }
 
+                // 
+                // level of responsiveness to req. info
+                //
+                // brw
+                let { date_received: brw_req, reponse_date: brw_res } = data[0]
+                $("#brw-req-date").text(brw_req);
+                $("#brw-res-date").text(brw_res);
+                $("#brw-duration").text(`${brw_duration} days`);
+
+                // ppdc
+                let { date_received: ppdc_req, reponse_date: ppdc_res } = data[1]
+                $("#ppdc-req-date").text(ppdc_req);
+                $("#ppdc-res-date").text(ppdc_res);
+                $("#ppdc-duration").text(`${ppdc_duration} days`);
+
+                // budgit
+                let { date_received: budgit_req, reponse_date: budgit_res } = data[2]
+
+                if (budgit_res === "0000-00-00") {
+                    budgit_res = "No response"
+                }
+                if (budgit_duration === "no") {
+                    budgit_duration = 0
+                }
+                $("#budgit-req-date").text(budgit_req);
+                $("#budgit-res-date").text(budgit_res);
+                $("#budgit-duration").text(`${budgit_duration} days`);
+
+                // 
+                // annual report
+                // 
+                function checkAnnualReport(mda, id) {
+                    if (mda === "published and submitted") {
+                        $(id).append('<li class="text-success">Published and Submitted</li>')
+                    } else if (mda === "submitted") {
+                        $(id).append('<li class="text-warning">Submitted</li>')
+                    } else if (mda === "no report") {
+                        $(id).append('<li class="text-danger">No report</li>')
+                    }
+                }
+                // brw
+                let { annual_report: brw_annual_report } = data[0]
+                checkAnnualReport(brw_annual_report, "#brw-annual-rpt");
+
+                // ppdc
+                let { annual_report: ppdc_annual_report } = data[1]
+                checkAnnualReport(ppdc_annual_report, "#ppdc-annual-rpt");
+
+                // budgit
+                let { annual_report: budgit_annual_report } = data[2]
+                checkAnnualReport(budgit_annual_report, "#budgit-annual-rpt");
 
             },
             error: e => console.log('error', e)
